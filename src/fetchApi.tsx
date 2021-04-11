53fd71ad521764628c8f750de514a1d5a89772bb
Rick Perez <Rickperez87@gmail.com>
2021-04-11 11:15:00 -0400

    fetch call for random recipe

--- src/fetchApi.tsx
+++ src/fetchApi.tsx
@@ -1,5 +1,10 @@
 const RANDOM_RECIPE_URL = "https://api.spoonacular.com/recipes/random";
 
 function fetchRandomRecipe() {
-  fetch(RANDOM_RECIPE_URL);
+  fetch(RANDOM_RECIPE_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
 }
