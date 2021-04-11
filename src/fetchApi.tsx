e3aae8d3a6763807cad564ff27900fc2ae1df523
Rick Perez <Rickperez87@gmail.com>
2021-04-11 11:06:21 -0400

    setup initial react component and fetchapi

---
+++ src/fetchApi.tsx
@@ -0,0 +1,5 @@
+const RANDOM_RECIPE_URL = "https://api.spoonacular.com/recipes/random";
+
+function fetchRandomRecipe() {
+  fetch(RANDOM_RECIPE_URL);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./components/App";
 
-const Application: React.SFC<{}> = () => <h1>Application</h1>;
-
-render(<Application />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
