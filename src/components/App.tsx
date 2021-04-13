bd688e71cde2ce298b0ebe06796d5b6dd7c10faf
Rick Perez <Rickperez87@gmail.com>
2021-04-13 12:48:39 -0400

    Install axios and setup recipe api call

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import fetchRandomRecipe from "../fetchApi";
-
-const [html, setHtml] = useState("");
-
-useEffect(() => {
-  const fetchRandom = async () => {
-    try {
-      const html = await fetchRandomRecipe();
-      if (html) setHtml(html);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-}, []);
+import axios from "axios";
 
 export default function App() {
+  const [data, setData]: any = useState("");
+  const [term, setTerm] = useState("");
+
+  const onSearchSubmit = async (term: string) => {
+    const RANDOM_RECIPE_URL =
+      "https://api.spoonacular.com/recipes/autocomplete";
+    // const apiKey = `?apiKey=${process.env.API_KEY}`;
+    const res = await axios.get(RANDOM_RECIPE_URL, {
+      params: { query: term, number: 10, apiKey: process.env.API_KEY },
+    });
+    setData(res);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearchSubmit(term);
+  };
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+    console.log(term);
+  };
   return (
     <>
       <h1>App</h1>
-      <button onClick={handleClick}>Search Recipes</button>
-      <div>{html}</div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+          value={term}
+        />
+        <button>Search Recipes</button>
+      </form>
     </>
   );
 }
