2876ac04d251c8fbfed905a78f5ca7ec9c470453
Rick Perez <Rickperez87@gmail.com>
2021-04-15 16:56:59 -0400

    sidebar items component and styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SidebarItem from "./SidebarItems/SidebarItem";
 import "./style";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      {/* {recipes.map(recipe=><SidebarItem recipe={recipe}/>)} */}
     </div>
   );
 };
---
+++ src/components/Sidebar/SidebarItems/SidebarItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SidebarItem() {
+  return (
+    <div className="sidebar-item">
+      {/* // <div className="sidebar-item__image">{recipes.image}</div>
+      // <div className="sidebar-item__title">{recipes.title}</div>
+      // <div className="sidebar-item__author">{recipes.author}</div> */}
+    </div>
+  );
+}
+
+export default SidebarItem;
