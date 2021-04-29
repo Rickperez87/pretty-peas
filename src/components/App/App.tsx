75faf902e705cd3d3d0e3a676bd89747664539fd
Rick Perez <Rickperez87@gmail.com>
2021-04-29 11:57:39 -0400

    rename App style sheet

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import MainContent from "../MainContent/MainContent";
-import "./base.scss";
+import "./style.scss";
+import "../../scss/main.scss";
 
 const App: React.FC<{}> = () => {
   return (
